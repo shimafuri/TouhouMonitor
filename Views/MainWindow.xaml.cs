9708f12c639fd081cf0870d78a56fdb03fecd729
Anonymous <user@example.com>
2020-08-31 14:57:37 +0900

    feat: create project with Prism Blank template.

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TouhouMonitor.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
